9243c98e700bdf35ea8227d6b384f4420ec47345
mubaarikmkm <idiriscali791@gmail.com>
2023-09-28 13:25:14 -0700

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-  echo "Please provide a URL as an argument"
-  exit 1
-fi
-
-url=$1
-
-# Send GET request and store the response in a variable
-response=$(curl -s -w "%{http_code}" $url)
-
-# Extract the status code from the response
-status_code=$(echo "$response" | tail -n 1)
-
-# Check if the status code is 200 (OK)
-if [ $status_code -eq 200 ]; then
-  # Extract and display the body of the response
-  body=$(echo "$response" | sed '$ d')
-  echo "$body"
-else
-  echo "Error: Request failed with status code $status_code"
-fi
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
 
